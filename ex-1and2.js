ae72d22ae419731b8e50ba4d05256749eb52f1ff
Ampare <visarut.thk@gmail.com>
2024-05-09 13:19:17 +0700

    Assignment-JS-Values-Variables

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let Name = "karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
+const RestaurantNames = "Chef's Table by Chef James";
+const tatalPrice = 15000;
+const restaurantOpennow = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”;
+tatalPrice = 5000;
+restaurantOpennow = 2500;
